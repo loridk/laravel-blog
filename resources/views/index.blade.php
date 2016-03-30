52f76fc3bf2e2550dfec8634788ade8a915f74ed
Lori Dunford <lori@redstairs.ca>
2016-03-30 12:59:11 -0400

    Route to index and homestead running

---
+++ resources/views/index.blade.php
@@ -0,0 +1,72 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+
+            <!-- Basic Page Needs
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <meta charset="utf-8">
+    <title>Red Stairs Blog</title>
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <!-- Mobile Specific Metas
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- FONT
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+
+
+    <!-- CSS
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+
+
+    <!-- Favicon
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <link rel="icon" type="image/ico" href="images/favicon.ico">
+
+</head>
+<body>
+
+<!-- Primary Page Layout
+–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+<div class="container">
+
+
+    <div class="row">
+
+
+
+
+Test
+
+
+
+
+
+
+
+    </div>
+
+
+
+</div>
+
+<footer>
+
+</footer>
+
+
+<!-- Scripts -->
+<script type="text/javascript" src="https://code.jquery.com/jquery-1.12.1.min.js"></script>
+<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
+
+
+</body>
+</html>
+
