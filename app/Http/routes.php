331da0a3deec2a17134df782bd9ab4789a24b578
Lori Dunford <lori@redstairs.ca>
2016-03-30 14:57:09 -0400

    Basic layouts

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -18,6 +18,10 @@ Route::group(['middleware' => ['web']], function () {
 
     Route::get('/', 'HomeController@index');
 
+    Route::get('/home', 'HomeController@index');
+
+    Route::get('/create', 'PostController@index');
+
 });
 
 
---
+++ resources/views/post.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-default">
+                <div class="panel-heading">New Post</div>
+
+                <div class="panel-body">
+
+
+                    <form role="form" id="post-form" class="form-horizontal" method="post" action="">
+
+                        <div class="form-group">
+                            <label class="control-label col-sm-2" for="post-title">Post Title:</label>
+                            <div class="col-sm-10">
+                                <input type="text" name="post-title" class="form-control" id="post-title" placeholder="Title" required>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label class="control-label col-sm-2" for="post-content">Post Content:</label>
+                            <div class="col-sm-10">
+                                <textarea class="form-control" name="post-content" rows="5" id="post-content" required></textarea>
+                            </div>
+                        </div>
+
+
+                        <div class="form-group">
+                            <div class="col-sm-offset-2 col-sm-10">
+                                <button type="submit" id="submit" class="btn btn-primary">Create Post</button>
+                            </div>
+                        </div>
+
+                    </form>
+
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
