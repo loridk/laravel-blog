a6508667d3dbd8982349b8b99ddf8d5c2fad0ce1
Lori Dunford <lori@redstairs.ca>
2016-04-05 15:26:03 -0400

    Trying to get delete to work

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -63,37 +63,15 @@ class PostController extends Controller
 
     }
 
-
-
-
-/*    public function store(Request $request, Post $post)
-    {
-        // validate
-        $this->validate($request, [
-            'body' => 'required|min:10',
-            'title' => 'required|min:4|max:140'
-        ]);
-
-        $post->create([
-            'title' => $request->title,
-            'body' => $request->body,
-            'user_id' => $request->user()->id
-        ]);
-
-
-        return view('home', 'PostController@index');
-    }*/
-
-
     public function destroy(Post $post)
     {
-        $post->delete();
+        $post-> delete();
 
         if($post->delete()) {
-            return Redirect::route('home');
+            return redirect('/');
         }
         else {
-            Echo "Error";
+            return "Error";
         }
     }
 
--- resources/views/new.blade.php
+++ resources/views/new.blade.php
@@ -40,21 +40,6 @@
 
                 </div>
             </div>
-            @if (count($errors) > 0)
-                    <!-- Form Error List -->
-            <div class="alert alert-danger">
-                <strong>Whoops! Something went wrong!</strong>
-
-                <br><br>
-
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-            @endif
-
         </div>
     </div>
 </div>
